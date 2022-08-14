7f456dae5067e80279e3e5b15856e5b0ed2ea253
gnehs <jayuiop@gmail.com>
2022-08-14 20:29:42 +0800

    fixed chrome height

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,7 +9,11 @@
 
 <script>
 export default {
-
+  created() {
+    // --vh
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
 </script>
 
@@ -20,13 +24,14 @@ export default {
 .mobile-view
   width: 100%
   margin: 0 auto
-  height: calc(100vh - 64px)
-  height: calc(100dvh - 64px)
+  height: calc(var(--vh,1vh) * 100 - 64px)
   background-color: #333
   color: #fff
   padding: 0 16px
   overflow: hidden
   overflow-y: scroll
+  @supports (height: 100dvh)
+    height: calc(100dvh - 64px)
   @media (min-width: 1024px)
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1)
     width: 512px
